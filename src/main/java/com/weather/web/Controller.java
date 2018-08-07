0e32ac3a69755677daf8f828a87cb44b1cba7c33
yangx18 <36057067+yangx18@users.noreply.github.com>
2018-08-07 01:11:15 -0500

    Add files via upload

---
+++ src/main/java/com/weather/web/Controller.java
@@ -0,0 +1,59 @@
+package com.weather.web;
+
+import com.weather.domain.DailyMessageWeatherResponse;
+import com.weather.service.WeatherServiceImpl;
+import com.github.fedy2.weather.data.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import java.util.Locale;
+
+@Controller
+public class Controller {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private IWeatherService<Channel> yahooService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "redirect:/forecast/lincoln";
+    }
+
+    @RequestMapping(value = "/forecast/{city}", method = RequestMethod.GET)
+    public String getForecast(Model model, @PathVariable("city") String city) throws Exception {
+
+        Channel yahooResponse = yahooService.getForecast(city);
+        model.addAttribute("cityKey", city);
+        model.addAttribute("currentWeather", yahooResponse.getItem().getCondition());
+        model.addAttribute("forecasts", yahooResponse.getItem().getForecasts());
+
+        return ResponseEntity.ok(weatherService.getDailytForecast(city));
+
+    }
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "FAQ";
+    }
+
+    @GetMapping("/singlesource")
+    public String singleSource() {
+        return "singlesource";
+    }
+
+    @GetMapping("/history")
+    public String history() {
+        return "history";
+    }
+    
+    @ExceptionHandler(value = Exception.class)
+    public String handleException(Exception e){
+        return Pages.ERROR;
+    }
+
+}
---
+++ src/main/java/com/weather/web/Pags.java
@@ -0,0 +1,11 @@
+package com.weather.web;
+
+import java.util.HashMap;
+
+public class Pages {
+
+    public static final String INDEX = "index";
+    //public static final String FORECAST = "forecast";
+    public static final String ERROR = "error";
+
+}
