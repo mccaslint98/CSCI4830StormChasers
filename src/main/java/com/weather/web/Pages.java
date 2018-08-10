1264cf0fcceda628a8f266dce971285eb8d5d215
yangx18 <36057067+yangx18@users.noreply.github.com>
2018-08-10 14:04:51 -0500

    Update and rename Pags.java to Pages.java

--- src/main/java/com/weather/web/Pages.java
+++ src/main/java/com/weather/web/Pages.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Pages {
 
     public static final String INDEX = "index";
-    //public static final String FORECAST = "forecast";
+    public static final String FORECAST = "forecast";
     public static final String ERROR = "error";
 
 }
