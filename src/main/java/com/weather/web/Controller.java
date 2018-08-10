431cfdaffbc40eae739604c3c8d7a5427c2d3277
yangx18 <36057067+yangx18@users.noreply.github.com>
2018-08-10 14:01:19 -0500

    Update Controller.java

--- src/main/java/com/weather/web/Controller.java
+++ src/main/java/com/weather/web/Controller.java
@@ -17,7 +17,7 @@ public class Controller {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private WeatherService<Channel> yahooService;
+    private WeatherService<Channel> YahooService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
