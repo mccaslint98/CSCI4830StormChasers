7508963bc9b0c3b76b9e9ac77f92ec96ac1476f5
yangx18 <36057067+yangx18@users.noreply.github.com>
2018-08-11 16:52:49 -0500

    Add files via upload

--- src/main/java/com/weather/service/YahooService.java
+++ src/main/java/com/weather/service/YahooService.java
@@ -2,15 +2,26 @@ package com.weather.service;
 
 import com.github.fedy2.weather.YahooWeatherService;
 import com.github.fedy2.weather.data.Channel;
+import com.github.fedy2.weather.data.Condition;
+import com.github.fedy2.weather.data.Item;
 import com.github.fedy2.weather.data.unit.DegreeUnit;
+import java.util.Date;
 import java.util.HashMap;
 import javax.xml.bind.JAXBException;
 import org.springframework.stereotype.Service;
+import com.weather.dao.CurrentData;
+import com.weather.dao.DailyData;
+import com.weather.dao.WeeklyData;
 
 @Service
 public class YahooService extends YahooWeatherService { //implements WeatherService<Channel> {
-
+    
     private static final HashMap<String, String> VOEIDS;
+    CurrentData cur;
+    DailyData day;
+    WeeklyData week;
+    
+    
 
     static {
 
@@ -25,10 +36,18 @@ public class YahooService extends YahooWeatherService { //implements WeatherServ
     }
 
     public Channel getDailytForecast(String city) throws Exception {
+        
+        
+       Channel ch; 
+        ch = getForecast(VOEIDS.get(city), DegreeUnit.FAHRENHEIT);
+  
+        return cur;
+       }
+     public Channel getItem() throws Exception {
+         Channel it;
+        it = getItem();
+         return it;
+     }
 
-        Channel ch = getForecast(VOEIDS.get(city), DegreeUnit.CELSIUS);
-        return ch;
-
-    }
 
 }
