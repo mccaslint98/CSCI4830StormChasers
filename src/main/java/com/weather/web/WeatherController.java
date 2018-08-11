63f3d817b9f74073ac9bdc9a36c85831e3a19b5d
yifanshi86 <yifanshi@unomaha.edu>
2018-08-10 19:41:19 -0500

    current temperature displays correctly on home page.

--- src/main/java/com/weather/web/WeatherController.java
+++ src/main/java/com/weather/web/WeatherController.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -24,36 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WeatherController {
 
-    //private WeatherServlet wServlet = new WeatherServlet();
-//    @RequestMapping("/#current")
-//    public String home(Map<String, Object> model){
-//        model.put("message", "Current");
-//        return "Current.html";
-//    }
-//
-//    @PostMapping("/daily")
-//    public void daily(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp)
-//    throws ServletException, IOException {
-//        model.put("message", "Daily Page");
-//
-//        String city = req.getParameter("city");
-//        String source = req.getParameter("source");
-//        System.out.println(city);
-//
-//        WeatherDAO weatherDao = new WeatherDAO();
-//        ArrayList<Weather> weatherList = weatherDao.getWeather(city,source);
-//        req.setAttribute("weatherList",weatherList);
-//        RequestDispatcher rd=req.getRequestDispatcher("daily.jsp");
-//        rd.forward(req, resp);
-//
-//    }
-//    @Autowired
-//    WeatherServiceImpl weatherService;
-//
-//    @RequestMapping("/daily/{city}")
-//    public ResponseEntity<DailyMessageWeatherResponse> getDaily(@PathVariable String city) {
-//        return ResponseEntity.ok(weatherService.getDailytForecast(city));
-//    }
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -65,16 +34,13 @@ public class WeatherController {
 //    }
     //@RequestMapping("/forecast/{city}")
     @RequestMapping("/")
-    public String getForecast(Model model, @PathVariable("city") String city) throws Exception {
-        //public String getForecast(@PathVariable("city") String city) throws Exception {
+    //public String getForecast(Model model, @PathVariable("city") String city) throws Exception {
+    //public String getForecast(@PathVariable("city") String city) throws Exception {
+    public String getForecast(Model model) throws Exception {
 
-        Channel yahooResponse = yahooService.getDailytForecast(city);
-        model.addAttribute("cityKey", city);
-        model.addAttribute("currentWeather", yahooResponse.getItem().getCondition());
-        model.addAttribute("forecasts", yahooResponse.getItem().getForecasts());
-
-        //return yahooResponse.toString();
-        return city + "not available!";
+        Channel yahooResponse = yahooService.getDailytForecast("omaha");
+        model.addAttribute("curTemp", yahooResponse.getItem().getCondition().getTemp() * 1.8 + 32);
+        return "home";
     }
 
     @GetMapping("/faq")
